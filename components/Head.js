db0c115705fc69ca12913aeaa8094b1b5291d7ff
Nikolai <kirichk@gmail.com>
2022-03-24 22:27:10 +0200

    slight changes v2

--- components/Head.js
+++ components/Head.js
@@ -4,7 +4,7 @@ import NextHead from "next/head";
 const Head = ({ title, description }) => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{title || ""}</title>
+    <title>The Storyblok Herald</title>
     <meta name="description" content={description || ""} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
   </NextHead>
